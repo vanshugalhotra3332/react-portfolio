c862a0b4653071dfe8df276866f7671b03859a81
Vanshu Galhotra <vanshugalhotra3332@gmail.com>
2022-09-19 13:52:07 +0530

    added testimonials component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./Components/Home/Home";
 import Qualification from "./Components/Qualification/Qualification";
 import Services from "./Components/Services/Services";
 import Skills from "./Components/Skills/Skills";
+import Testimonials from "./Components/Testimonials/Testimonials";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Testimonials />
       </main>
     </div>
   );
---
+++ src/Components/Testimonials/Data.js
@@ -0,0 +1,25 @@
+import Eazy from "../../Assets/Images/eazy.jpg";
+
+export const Data = [
+  {
+    id: 1,
+    image: Eazy,
+    title: "Eazy E",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 2,
+    image: Eazy,
+    title: "Eazy E",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 3,
+    image: Eazy,
+    title: "Eazy E",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+];
